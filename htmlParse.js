714df9fe397855ef67b7204c3e8e1dae12f52aeb
夏天 <xiaqiang@xiaofaai.com>
2021-05-28 11:58:21 +0800

    优化代码 新增支持cosplay 二级目录内容下载

--- htmlParse.js
+++ htmlParse.js
@@ -1,44 +1,60 @@
 const https = require('https');
 const fs = require('fs');
 const chreeio = require('cheerio')
-const baseUrl = 'https://dl.ixxcc.com/images/'
-const category = [
-    'ElyEE_E子', // index 0
-    '一只云烧', // index 1
-    '九曲Jean', // index 2
-    '你的负卿', // index 3
-    '半半子', // index 4
-    '南桃Momoko', // index 5
-    '周叽是可爱兔兔', // index 6
-    '小野妹子', // index 7
-    '弥音音', // index 8
-    '念念_D', // index 9
-    '抖娘利世', // index 10
-    '水淼aqua', // index 11
-    '疯猫ss', // index 12
-    '白金Saki', // index 13
-    '秋和柯基', // index 14
-    '腐团儿', // index 15
-    '菌烨Tako', // index 16
-    '蜜汁猫裘', // index 17
-    '蠢沫沫', // index 18
-    '起司块wii', // index 19
-    '过期米线线喵', // index 20
-    '魔物喵Nagisa', // index 21
-    '黑川' // index 22
-]
-let categoryIndex = 0
+const BaseUrl = 'https://dl.ixxcc.com/images/'
+let categoryIndex = 0;
 
-// 创建目录 跑一边足够
-category.forEach(i=>{
-    fs.mkdir(`./imagePath/${i}`, { recursive: true }, (err) => {
-        if (err) throw err;
-    });
+// 读取目录 写入category.json目录部分的代码省略
+ function readCategory(readPath,writePath){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(readPath,'utf-8',(err,data)=>{
+            if(err){
+                console.log('文件读取失败')
+                reject('文件读取失败')
+            } else {
+                const category = JSON.parse(data)
+                resolve(category)
+                // 创建目录 跑一边足够
+                // category.forEach(i=>{
+                //     let path = ''
+                //     if (i.indexOf('/')> -1){
+                //         path = i.split('/')[0]
+                //     } else {
+                //         return
+                //     }
+                //     fs.mkdir(`${writePath}${path}`, { recursive: true }, (err) => {
+                //         if (err) throw err;
+                //     });
+                // })
+            }
+        })
+    })
+}
+
+const downloadPart = [{
+    writePath:'./imagePath/',
+    readPath: './imagePath/category.json',
+    baseUrl:BaseUrl
+},{
+    writePath:'./imagePath/cosplay/',
+    readPath: './imagePath/category1.json',
+    baseUrl:'https://dl.ixxcc.com/images/cosplay/'
+}]
+// 启动器 读取路径 下载文件 第一次跑需要加上writePath 将写入目录部分代码注释打开
+const part = 1; // 0/1  0 下载一级目录内容  1下载二级目录内容
+const {readPath,writePath,baseUrl} = downloadPart[part]
+readCategory(readPath,writePath).then(category=>{
+    downloadHtml(category,baseUrl)
 })
-downloadHtml()
-function downloadHtml(){
+/*
+    category 文件目录
+    baseUrl 路径
+    downloadType 1 单线  2 并行  默认单线
+ */
+function downloadHtml(category,baseUrl,downloadType = 1){
     const filePath = category[categoryIndex]
-    const url = baseUrl + filePath + '/'
+    const Slash = filePath.indexOf('/') > -1 ? '' : '/'
+    const url = baseUrl + filePath + Slash
     console.log(url,'开始爬取网页链接')
     https.get(url, function(res) {
         let data = "";
@@ -54,25 +70,35 @@ function downloadHtml(){
                     imgArr.push(href)
                 }
             })
-            downloadImg(imgArr,filePath)
-            if(categoryIndex < 22){
-                categoryIndex++
-                downloadHtml()
+            const path = baseUrl.replace(BaseUrl,'') + filePath.replace('/','');
+            // 下载方式 二选一即可 这里选择单线下载
+            if(downloadType === 1){ // 单线下载 下载图片方法需加上category,baseUrl 参数 并行下载需要去掉这两个参数
+                downloadImg(imgArr,path,category,baseUrl)
+            } else { // 并行下载 有内存溢出风险
+                downloadImg(imgArr,path)
+                if(categoryIndex < category.length){
+                    categoryIndex++
+                    downloadHtml(category,baseUrl)
+                }
             }
+
+
+
         });
     }).on("error", function() {
         console.log('获取网页失败')
     });
 }
-function downloadImg(imgArr,filePath){
+function downloadImg(imgArr,filePath,category,baseUrl){
     console.log(`开始下载${filePath}部分内容`)
     let imgArrIndex = 0
     function loop(){
         const img = imgArr[imgArrIndex].split('./')[1]
-        const url = baseUrl + filePath + '/' + img
+        const url = BaseUrl + filePath + '/' + img
         const arr = url.split('/')
         const name = arr[arr.length-1].split('.jpg')[0]
         https.get(url, function(res) {
+            console.log(url,'图片路径')
             let imgData = "";
             //设置图片编码格式
             res.setEncoding("binary");
@@ -83,6 +109,12 @@ function downloadImg(imgArr,filePath){
             res.on("end", function() {
                 if(imgArrIndex >= imgArr.length -1){
                     console.log(`${filePath}部分内容下载完璧`)
+                    if(category && baseUrl){
+                        if(categoryIndex < category.length){
+                            categoryIndex++
+                            downloadHtml(category,baseUrl)
+                        }
+                    }
                 } else {
                     imgArrIndex++
                     loop()
